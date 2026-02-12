b58a4d8f32e194328e2ee6b9c4261d8eb2140385
Nick <archon04@mail.ru>
2026-02-12 14:04:31 +0200

    init

---
+++ src/main/java/esg/esgdocbuilder/EsgDocBuilderApplication.java
@@ -0,0 +1,13 @@
+package esg.esgdocbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgDocBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsgDocBuilderApplication.class, args);
+    }
+
+}
