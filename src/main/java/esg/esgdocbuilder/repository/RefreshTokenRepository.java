12d2bea876f20460f1eb3eba9a304902e0eb2669
NikitaYatsko <archon04@mail.ru>
2026-03-28 15:10:37 +0200

    added pagination for bank operations

--- src/main/java/esg/esgdocbuilder/repository/RefreshTokenRepository.java
+++ src/main/java/esg/esgdocbuilder/repository/RefreshTokenRepository.java
@@ -2,18 +2,12 @@ package esg.esgdocbuilder.repository;
 
 import esg.esgdocbuilder.model.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(Long userId);
 
-    @Modifying
-    @Query("DELETE FROM RefreshToken r WHERE r.token = :token")
-    void deleteByToken(@Param("token") String token);
 
     Optional<RefreshToken> findByToken(String token);
 }
