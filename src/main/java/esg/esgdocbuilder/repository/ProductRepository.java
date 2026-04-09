f5318d0ee7aa12125a1517bf09ff9c73a101085b
Gilver1945 <cheban.ivan.2021@bk.ru>
2026-04-09 18:18:31 +0300

    Added powerKwt value to invoice and added power value to PDF file.

--- src/main/java/esg/esgdocbuilder/repository/ProductRepository.java
+++ src/main/java/esg/esgdocbuilder/repository/ProductRepository.java
@@ -4,6 +4,8 @@ import esg.esgdocbuilder.model.entity.Product;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Override
     @EntityGraph(attributePaths = "category")
