11f3c9c7603d90d19cd5a3509fda7ddd7da8773d
NikitaYatsko <archon04@mail.ru>
2026-04-11 16:21:00 +0300

    created products pdf printing, small bug fixed

--- src/main/java/esg/esgdocbuilder/repository/UserRepository.java
+++ src/main/java/esg/esgdocbuilder/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package esg.esgdocbuilder.repository;
 
 import esg.esgdocbuilder.model.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    @EntityGraph(attributePaths = {"roles"})
+    List<User> findAll();
 
     boolean existsByEmail(String email);
 }
